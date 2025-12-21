fe79fecba879d1e055b73cbc7b3c040bb66f7fb2
John Hooks <bitmachina@outlook.com>
2025-12-21 12:44:20 -0800

    refactor: clean up types and remove dead code
    Remove unused groupBy function, fix redundant optional chaining,
    simplify TypeScript config. Add design doc for future theme metadata.

--- src/highlighter/utils.ts
+++ src/highlighter/utils.ts
@@ -1,26 +1,3 @@
-/**
- * Copy of internal utils not exported by `shiki`.
- *
- * [shiki/src/utils.ts](https://github.com/shikijs/shiki/blob/a585c9d6860334a6233ff1c035a42d023e016400/packages/shiki/src/types.ts#L203-L242)
- */
-
-export function groupBy<TElement, TKey>(
-  elements: TElement[],
-  keyGetter: (element: TElement) => TKey
-): Map<TKey, TElement[]> {
-  const map = new Map<TKey, TElement[]>();
-  for (const element of elements) {
-    const key = keyGetter(element);
-    if (map.has(key)) {
-      const group = map.get(key);
-      group?.push(element);
-    } else {
-      map.set(key, [element]);
-    }
-  }
-  return map;
-}
-
 const htmlEscapes = {
   "&": "&amp;",
   "<": "&lt;",
