0ac4cd6bf3a4310f2d09c49d60c3504751dd920b
John Hooks <bitmachina@outlook.com>
2025-12-21 10:14:56 -0800

    chore: replace rollup with tsup build system

--- scripts/build-docs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if ! [ -e ./temp/highlighter.api.json ] ; then
-  echo "api.json file missing, building package..."
-  ./scripts/build-pkg.sh
-fi
-
-
-./node_modules/.bin/api-documenter markdown -i ./temp -o ./docs
